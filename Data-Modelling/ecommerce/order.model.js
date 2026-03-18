54f3944774db0660ad5ec85b97a286602c99cb91
Ganesh.H <hganesh1909@gmail.com>
2026-03-18 12:36:10 +0530

    feat: add Order model and update Product model with owner field

---
+++ Data-Modelling/ecommerce/order.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+  customer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  products: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        default: 1,
+      },
+    },
+  ],
+  totalPrice: {
+    type: Number,
+    default: 0,
+  },
+});
+
+export const Order = mongoose.model("Order", orderSchema);
--- Data-Modelling/ecommerce/product.model.js
+++ Data-Modelling/ecommerce/product.model.js
@@ -24,7 +24,13 @@ const productSchema = new mongoose.Schema(
     category: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Category",
+      required : true
+
     },
+    owner:{
+      type: mongoose.Schema.Types.ObjectId,
+      ref : "User"
+    }
   },
   { timestamps: true },
 );

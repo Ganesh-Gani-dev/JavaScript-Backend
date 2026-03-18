63517ad03c248fbc5145d6b00a0c737965e7d431
Ganesh.H <hganesh1909@gmail.com>
2026-03-18 12:07:02 +0530

    Add package.json for Data-Modelling with express dependency

---
+++ Data-Modelling/ecommerce/product.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mangoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    description: {
+      required: true,
+      type: String,
+    },
+    name: {
+      required: true,
+      type: String,
+    },
+    productImage: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    stock: {
+      default: 0,
+      type: Number,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    },
+  },
+  { timestamps: true },
+);
+
+export const Product = mongoose.model("Product", productSchema);
